7234d43093e61cde7cd0436f28ecb1db0df44089
baptxste <baptiste.chachura@me.com>
2025-02-12 22:07:20 +0100

    nb partie1

--- partie2/TPRU.py
+++ partie2/TPRU.py
@@ -9,7 +9,9 @@ import torch.nn.functional as F
 class TPRUEncoder(nn.Module):
     def __init__(self, d, d_prime, N): 
         super().__init__()
-        self.d, self.d_prime, self.N = d, d_prime, N
+        self.d = d
+        self.d_prime = d_prime
+        self.N = N
 
         self.Wu = nn.Parameter(torch.randn(d, d))
         self.Wr = nn.Parameter(torch.randn(d, d))
@@ -55,71 +57,3 @@ class TPRUEncoder(nn.Module):
             bt = (gt * torch.tanh(b) + (1 - gt) * bt.T).T  # (batch_size, N)
         #print(bt.shape)
         return bt  # (batch_size, N)
-
-# class TPRUEncoder(nn.Module):
-#     def __init__(self, d, d_prime, N): 
-#         """ 
-#         d : hidden size
-#         d_prime : 1 pour les chiffres
-#         N : nombres de fillers, roles
-#         """
-#         super().__init__()
-#         self.d, self.d_prime, self.N = d, d_prime, N
-
-#         self.Wu = nn.Parameter(torch.randn(d, d))
-#         self.Wr = nn.Parameter(torch.randn(d, d))
-#         self.Wb = nn.Parameter(torch.randn(d, d))
-#         self.W = nn.Parameter(torch.randn(d, d_prime))
-#         self.Wx = nn.Parameter(torch.randn(d, d_prime))
-
-#         self.register_buffer("V", torch.randn(d, N)) #V fixe
-
-#     def forward(self, sequence):
-#         if sequence.dim() == 1:  
-#             sequence = sequence.unsqueeze(1).float() #(len(seq),) -> (len(seq),1)
-#         else:
-#             sequence = sequence.float()     
-#         bt = torch.zeros(self.d, 1)  
-#         for xt in sequence:  
-#             #print("xt:", xt)
-#             xt = xt.view(1, 1)
-#             #print("xt:", xt.shape)
-#             U = self.Wu @ self.V #dxN
-#             #print("U:", U.shape)
-#             fxt = F.relu(U.T @ self.W @ xt) 
-#             #print("fxt:", fxt.shape)
-#             fbt = F.relu(U.T @ bt) 
-#             #print("fbt:", fbt.shape) 
-#             numerator = (fbt + fxt) ** 2
-#             denominator = torch.sum(numerator)
-#             ft = numerator / denominator  
-#             R = self.Wr @ self.V 
-#             b = R @ ft  
-#             gt = torch.sigmoid(self.Wb @ bt + self.Wx @ xt) 
-#             bt = gt * torch.tanh(b) + (1 - gt) * bt  #dx1
-#         return bt
-
-
-
-# d = 32 #hidden size
-# d_prime = 1 #chiffre
-# N = 5 #5 fillers par chiffre de la séquence
-# encoder = TPRUEncoder(d, d_prime, N)
-# input = torch.tensor([1,3,4])
-# print(encoder(input).shape)
-
-
-
-# N = 5  
-# fxt = torch.tensor([1,0,2])  # Nx1
-# fbt = torch.tensor([1,2,1])  # Nx1
-# numerator = (fbt + fxt)**2  # Nx1
-
-# denominator = torch.sum(numerator)  # Scalaire
-
-# # Diviser chaque composante par la somme pour obtenir ft
-# ft = (numerator / denominator)  # Nx1
-
-# print(numerator)
-# print(denominator)
-# print(ft)
